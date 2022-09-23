551cef71d41713655822c9e0f4a814ae42153e5f
Alberto de Olivira <betodeoliveira@outlook.com>
2022-09-22 21:26:24 -0300

    Link Blocks

--- colorful-links.js
+++ colorful-links.js
@@ -1,1 +1,19 @@
+let originalTextColor;
+let originalBackgroundColor;
 
+$("a.colorful-link-block").mouseenter(function () {
+    originalTextColor = $(this).css("color");
+    originalBackgroundColor = $(this).css("background-color");
+
+    let availableColors = $(".colorful-link-block-list").children().length;
+    let randomNum = Math.floor(Math.random() * availableColors);
+    let selectedTextColor = $(".colorful-link-block-item").eq(randomNum).css("color");
+    let selectedBackgroundColor = $(".colorful-link-block-item").eq(randomNum).css("background-color");
+
+    $(this).css("color", selectedTextColor);
+    $(this).css("background-color", selectedBackgroundColor);
+    
+}).mouseleave(function () {
+    $(this).css("color", originalTextColor);
+    $(this).css("background-color", originalBackgroundColor);
+});
